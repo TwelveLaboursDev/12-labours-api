834a8ed1a81b2f405063e66f4bc2cf29320224b3
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-08-28 13:57:01 +1200

    add expose_headers

--- app/main.py
+++ app/main.py
@@ -47,7 +47,8 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
+    expose_headers=["X-File-Name"]
 )
 
 SUBMISSION = None
