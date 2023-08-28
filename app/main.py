a2650a0e045247339711218b797110821ec6a8fb
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-08-28 13:11:51 +1200

    remove expose_headers

--- app/main.py
+++ app/main.py
@@ -47,8 +47,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["x-file-name"]
+    allow_headers=["*"]
 )
 
 SUBMISSION = None
