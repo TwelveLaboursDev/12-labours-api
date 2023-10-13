4eb26befb8084a53fce459c97cec264df7600c00
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-10-13 15:45:41 +1300

    create parent formatter

---
+++ app/function/formatter.py
@@ -0,0 +1,131 @@
+import json
+import re
+
+
+class Formatter:
+    def handle_thumbnail(self, data):
+        """
+        Handler for updating thumbnail
+        """
+        result = []
+        if data == []:
+            return result
+        for _ in data:
+            if _["additional_types"] is None:
+                result.append(_)
+        return result
+
+    def _handle_multiple_cite_path(self, filename, cite):
+        """
+        Handler for updating multiple cite path
+        """
+        full_path = ""
+        result = {"path": [], "relative": {"path": []}}
+        data = json.loads(re.sub("'", '"', cite))
+        for _ in data:
+            full_path_list = filename.split("/")
+            full_path_list[-1] = _.split("/")[-1]
+            full_path = "/".join(full_path_list)
+            result["path"].append(full_path)
+            result["relative"]["path"].append(_.split("/")[-1])
+        return result
+
+    # filename: (contains full file path), cite: isDerivedFrom/isDescribedBy/isSourceOf
+    def _handle_cite_path(self, filename, cite):
+        """
+        Handler for updating cite path
+        """
+        cite_path = self._handle_empty(cite)
+        full_path = ""
+        result = {"path": [], "relative": {"path": []}}
+        if cite_path != "":
+            if len(cite_path.split(",")) > 1:
+                result = self._handle_multiple_cite_path(filename, cite_path)
+                return result
+            full_path_list = filename.split("/")
+            full_path_list[-1] = cite_path.split("/")[-1]
+            full_path = "/".join(full_path_list)
+        result["path"].append(full_path)
+        result["relative"]["path"].append(cite_path.split("/")[-1])
+        return result
+
+    def _handle_empty(self, data):
+        """
+        Handler for updating empty value
+        """
+        if data is None or data == "NA":
+            return ""
+        return data
+
+    def _handle_image_link(self, preview, filename, is_source_of, has_image):
+        """
+        Handler for updating the image url
+        """
+        result = preview
+        if has_image:
+            if is_source_of != "":
+                path_list = filename.split("/")
+                path_list[-1] = is_source_of.split("/")[-1]
+                filepath = "/".join(path_list)
+                result += filepath
+            else:
+                result += filename
+        else:
+            return ""
+        return result
+
+    def handle_manifest(self, uuid, preview, data, has_image=False):
+        """
+        Handler for updating the data format which be queried based on manifest structure
+        """
+        result = []
+        for _ in data:
+            filename = _["filename"]
+            item = {
+                "image_url": self._handle_image_link(
+                    preview,
+                    filename,
+                    self._handle_empty(_["is_source_of"]),
+                    has_image,
+                ),
+                "additional_mimetype": {
+                    "name": self._handle_empty(_["additional_types"])
+                },
+                "datacite": {
+                    "isDerivedFrom": self._handle_cite_path(
+                        filename, _["is_derived_from"]
+                    ),
+                    "isDescribedBy": self._handle_cite_path(
+                        filename, _["is_described_by"]
+                    ),
+                    "isSourceOf": self._handle_cite_path(filename, _["is_source_of"]),
+                    "supplemental_json_metadata": {
+                        "description": self._handle_empty(
+                            _["supplemental_json_metadata"]
+                        )
+                    },
+                },
+                "dataset": {
+                    "identifier": uuid,
+                    "path": filename,
+                },
+                "file_type": {
+                    "name": self._handle_empty(_["file_type"]),
+                },
+                "identifier": _["id"],
+                "name": filename.split("/")[-1],
+            }
+            result.append(item)
+        return result
+
+    def handle_contributor(self, data):
+        """
+        Handler for updating the contributor format
+        """
+        result = []
+        if data == []:
+            return result
+        for _ in data:
+            contributor = {"name": _}
+            result.append(contributor)
+        return result
